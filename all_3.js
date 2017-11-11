919e8b996030a2287cc8b3db49d1203365cc8c67
agathegrisard <32573778+agathegrisard@users.noreply.github.com>
2017-11-11 16:46:41 +0100

    Delete all_3.js

--- all_3.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['isrefractory',['isRefractory',['../classNeuron.html#a1c42205232c387e6e48fd7a34917a4a9',1,'Neuron']]],
-  ['isspiking',['isSpiking',['../classNeuron.html#a80bb77af9e3d56e6986e0d78b8ba1010',1,'Neuron']]]
-];
